e95f4f17dee7b22115d0c32c7aef6a63a060a135
Côme Perrot <cperrot@laas.fr>
2023-02-10 18:19:29 +0100

    Added function to represent problem generated by crocoddyl

--- mpc-pointing/src/ocp-debug.cpp
+++ mpc-pointing/src/ocp-debug.cpp
@@ -35,11 +35,61 @@ std::vector<OCP_debugData> OCP_Point::fetchFromFile(std::string name) {
   return (datas);
 }
 
-void OCP_Point::reprOCP(){
+void OCP_Point::reprOCP() {
   auto modelCosts = costs(0)->get_costs();
 
-  for (auto cost: modelCosts){
-    std::cout << cost.second->name << cost.second->active << cost.second->weight << std::endl;
+  for (auto cost : modelCosts) {
+    auto second = cost.second;
+
+    std::cout << cost.second->name << std::endl;
+    std::cout << "  active: " << cost.second->active << std::endl;
+    std::cout << "  weight: " << cost.second->weight << std::endl;
+
+    std::cout << "  ";
+    second->cost->print(std::cout);
+    std::cout << std::endl;
+
+    // Residual
+    auto residual = second->cost->get_residual();
+
+    // actuation task
+    auto actuationResidual =
+        boost::dynamic_pointer_cast<crocoddyl::ResidualModelControl>(residual);
+    if (actuationResidual != NULL) {
+      std::cout << "    Residual reference: ";
+      std::cout << actuationResidual->get_reference().transpose() << std::endl;
+    }
+
+    // posture task
+    auto postureResidual =
+        boost::dynamic_pointer_cast<crocoddyl::ResidualModelState>(residual);
+    if (postureResidual != NULL) {
+      std::cout << "    Residual reference: ";
+      std::cout << postureResidual->get_reference().transpose() << std::endl;
+    }
+
+    // Activation
+    auto activation = second->cost->get_activation();
+
+    auto weightedQuad =
+        boost::dynamic_pointer_cast<crocoddyl::ActivationModelWeightedQuad>(
+            activation);
+    if (weightedQuad != NULL) {
+      std::cout << "    Activation weights: ";
+      std::cout << weightedQuad->get_weights().transpose() << std::endl;
+    }
+
+    auto quadBarrier =
+        boost::dynamic_pointer_cast<crocoddyl::ActivationModelQuadraticBarrier>(
+            activation);
+    if (quadBarrier != NULL) {
+      std::cout << "    beta: ";
+      std::cout << quadBarrier->get_bounds().beta << std::endl;
+      std::cout << "    lower bound: ";
+      std::cout << quadBarrier->get_bounds().lb.transpose() << std::endl;
+      std::cout << "    upper bound: ";
+      std::cout << quadBarrier->get_bounds().ub.transpose() << std::endl;
+    }
   }
 }
 
